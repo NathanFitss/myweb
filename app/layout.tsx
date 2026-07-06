5f9dedc134706108443ffe16b465348bcb4f3b16
NathanFitss <nathanstudiooo@gmail.com>
2026-07-06 07:02:22 +0700

    layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "NathanFits",
+  description: "Pilih kebutuhan Anda di sini.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id">
+      <body className={inter.variable}>
+        {children}
+      </body>
+    </html>
+  );
+      }
