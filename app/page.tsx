a30d02685bc87bbdd7a5120bf71c18d8f859a7b4
NathanFitss <nathanstudiooo@gmail.com>
2026-07-06 07:04:25 +0700

    page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+
+      <h1 className="text-5xl font-semibold">
+        NathanFits
+      </h1>
+
+    </main>
+  );
+}
